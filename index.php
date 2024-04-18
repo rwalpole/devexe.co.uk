228ad4ffd4e7256ede05d8319d4d22ab1fde8a9d
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2024-04-18 17:47:59 +0100

    Correct CSS path

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto"/>
-    <link rel="stylesheet" href="css/style2.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
     <link rel='shortcut icon' href='images/favicon.ico' type='image/x-icon' />
   </head>
   <body onscroll="w3_close()">
